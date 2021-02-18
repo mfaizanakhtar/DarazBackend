eff6c064ee046535293d07aafb37b4a7e64ba161
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-02-19 03:59:29 +0500

    Date updated aws

--- routes/orders.js
+++ routes/orders.js
@@ -155,7 +155,7 @@ router.get('/allstats/:day',async(req,res)=>{
     date.setDate(date.getDate()-req.params.day);
     // console.log(date);
     date.setHours(0,0,0,0);
-    date.setHours(date.getHours()+5)
+    // date.setHours(date.getHours()+5)
     // console.log(date);
     enddate = new Date();
     if(req.params.day<=1){
