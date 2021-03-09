5c592ee179cecff0e95bdab414eb38fcfea3d14f
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-03-09 11:34:26 +0500

    Date Selector Added

--- routes/orders.js
+++ routes/orders.js
@@ -248,7 +248,7 @@ router.get('/allstats/:startdate/:enddate',async(req,res)=>{
     startdate.setHours(startdate.getHours()+5);
     enddate = new Date(req.params.enddate);
     enddate.setHours(23,59,59,59);
-    // enddate.setHours(enddate.getHours()+5);
+    enddate.setHours(enddate.getHours()+5);
 
     }
     await timezone();
