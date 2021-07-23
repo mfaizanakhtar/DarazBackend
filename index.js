c90243a47c1de5e638978d1acb4a197ad157d955
mfaizanakhtar <mfaizanakhtar@yahoo.com>
2021-07-23 19:46:51 +0500

    update

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.use(function(req, res, next) {
     next();
 })
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use('/api/darazapi',darazapi);
 app.use('/api/users',users);
 app.use('/api/auth',auth);
