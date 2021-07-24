1377ad617ea73a8ea49a36bee6af91cc81e51c17
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-07-24 13:37:50 +0500

    update

--- routes/orders.js
+++ routes/orders.js
@@ -143,7 +143,7 @@ router.post('/getLabelsData',auth,async(req,res)=>{
     await fetchLabelsAndUpdate(req.user.useremail)
     await Order.updateMany({OrderId:{$in:req.body.Orders}},{$set:{isPrinted:true}})
 
-        Order.find({OrderId:{$in:req.body.Orders}}).populate({path:'OrderItems',match:{ShippingType:'Dropshipping'}})
+        Order.find({OrderId:{$in:req.body.Orders}}).populate({path:'OrderItems',match:{ShippingType:'Dropshipping'}}).sort({"OrderItems.Sku":1})
         .then((response)=>{
             res.send(response)
         })
