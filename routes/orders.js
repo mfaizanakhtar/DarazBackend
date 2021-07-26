b325d58b166a9b6641b3f80b71f3dfb3c3c43a5a
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-07-26 14:41:03 +0500

    update

--- routes/orders.js
+++ routes/orders.js
@@ -149,7 +149,7 @@ router.post('/getLabelsData',auth,async(req,res)=>{
     // var LabelOrders
     var skuSort={}
     var shopSort={}
-    console.log(req.body.shopSort)
+    // console.log(req.body.shopSort)
     if(req.body.skuSort==true) {
         skuSort={"Skus":1}
     }
