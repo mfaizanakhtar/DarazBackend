45ebf7ed5d7bc2326b7ac46cfa376d6289daeebe
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-08-23 15:34:19 +0500

    update

--- index.js
+++ index.js
@@ -68,6 +68,9 @@ ShippingType:'Own Warehouse'},18000000);
 updateOrderItemStatus({},{$or:[{Status:'shipped'},{ Status:'ready_to_ship'},{ Status:'pending'}],
 ShippingType:'Dropshipping'},300000);
 
+updateOrderItemStatus({},{Status:'delivered',
+ShippingType:'Own Warehouse'})
+
 // labelFetch()
 // async function labelFetch(){
 
