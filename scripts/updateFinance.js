1057936c60a3578985d526b2d914eaa482eeb724
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-08-23 17:53:46 +0500

    update

--- scripts/updateFinance.js
+++ scripts/updateFinance.js
@@ -23,9 +23,9 @@ async function updateTransactions(){
                 //if not found, save into db
                 var transaction = getTransactionObj(t,shopid.useremail)
             transactResult = await transaction.save()
-            console.log(transactResult)
+            // console.log(transactResult)
             //find corresponding order and push transaction into order obj
-            OrderItems.update({OrderItemId:t["Order Item No."]},{
+            OrderItems.update({OrderItemId:transactResult.OrderItemNo},{
                 $push:{Transactions:transactResult._id}
             })
         }
