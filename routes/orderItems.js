3b3769ca3b9fd283d06f98ef28c4f49f4371f22e
mfaizanakhtar <79137097+mfaizanakhtar@users.noreply.github.com>
2021-09-22 18:47:58 +0500

    update

--- routes/orderItems.js
+++ routes/orderItems.js
@@ -102,7 +102,7 @@ router.get('/ordermovement/:filter',auth,async(req,res)=>{
         console.log('Beforestardate: ',startdate)
         console.log('Beforeenddate: ',enddate)
 
-        // startdate.setHours(0,0,0,0);
+        startdate.setHours(0,0,0,0);
         enddate.setHours(23,59,59,59);
         console.log('stardate: ',startdate)
         console.log('enddate: ',enddate)
