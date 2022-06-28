0accaa76d539b3d81bf5bfb0fcff3498d7a92f2f
mfaizanakhtar <mfaizanakhtar@yahoo.com>
2022-06-28 23:46:31 +0500

    logging for scheduler

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -17,6 +17,7 @@ function scheduler(){
     //new orders request cron at every 5minutes
     cron.schedule('*/5 * * * *',async ()=>{
         await updateOrdersData();
+        console.log("updateOrderItemStatus @ every 5 minutes")
     }, {
         scheduled: true,
         timezone: "Asia/Karachi"
@@ -25,6 +26,7 @@ function scheduler(){
     //new transaction request cron at every 1 hour
     cron.schedule('* */1 * * *',async ()=>{
         await updateTransactions();
+        console.log("updateOrderItemStatus @ every 1 hours")
     }, {
         scheduled: true,
         timezone: "Asia/Karachi"
@@ -33,6 +35,7 @@ function scheduler(){
     //update status request cron at every 8 hours
     cron.schedule('* */8 * * *',async ()=>{
         await updateOrderItemStatus({},{$or:[{Status:'shipped'},{ Status:'ready_to_ship'},{ Status:'pending'}],ShippingType:'Own Warehouse'});
+        console.log("updateOrderItemStatus @ every 8 hours")
     }, {
         scheduled: true,
         timezone: "Asia/Karachi"
@@ -49,16 +52,18 @@ function scheduler(){
     //update status request cron at every 15 minutes
     cron.schedule('*/15 * * * *',async ()=>{
         await updateOrderItemStatus({},{$or:[{Status:'pending'},{ Status:'ready_to_ship'}],ShippingType:'Dropshipping'});
+        console.log("updateOrderItemStatus @ every 12 minutes")
     }, {
         scheduled: true,
         timezone: "Asia/Karachi"
     })
 
-    //update status request cron at every 15 minutes
+    //update status request cron at every 8 hours
     cron.schedule('* */8 * * *',async ()=>{
         var startingDate=new Date();
         startingDate=startingDate.setDate(startingDate.getDate()-20)
         await updateOrderItemStatus({},{Status:'delivered',UpdatedAt:{$gte:startingDate}},8*60*60*1000);
+        console.log("updateOrderItemStatus @ every 8 hours")
 
     }, {
         scheduled: true,
@@ -69,6 +74,7 @@ function scheduler(){
     //update status request cron at every 12 hours
     cron.schedule('* */12 * * *',async ()=>{
         await getAllSkus()
+        console.log("getAllSkus @ every 12 hours")
     }, {
         scheduled: true,
         timezone: "Asia/Karachi"
