5cd5cfe72a7e5b91b68f5f1aa44867d08f77214f
mfaizanakhtar <mfaizanakhtar@yahoo.com>
2022-07-17 22:58:27 +0500

    fixed errors in scheduler routes

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -15,25 +15,25 @@ router.get('/updateOrdersData',async(req,res)=>{
 router.get('/updateTransactions',async(req,res)=>{
     console.log("updateTransactions api called")
     await updateTransactions();
-    res.status.send(200);
+    res.status(200).send();
 })
 
 router.get('/updateFbdOrderItemStatus',async(req,res)=>{
     console.log("updateFbdOrderItemStatus api called")
     await updateOrderItemStatus({},{$or:[{Status:'shipped'},{ Status:'ready_to_ship'},{ Status:'pending'}],ShippingType:'Own Warehouse'});
-    res.status.send(200);
+    res.status(200).send();
 })
 
 router.get('/updateFbmShippedItemStatus',async(req,res)=>{
     console.log("updateFbmShippedItemStatus api called")
     await updateOrderItemStatus({},{Status:'shipped',ShippingType:'Dropshipping'});
-    res.status.send(200);
+    res.status(200).send();
 })
 
 router.get('/updateQuickFbmOrderItemStatus',async(req,res)=>{
     console.log("updateQuickFbmOrderItemStatus api called")
     await updateOrderItemStatus({},{$or:[{Status:'pending'},{ Status:'ready_to_ship'}],ShippingType:'Dropshipping'});
-    res.status.send(200);
+    res.status(200).send();
 })
 
 router.get('/updateFbmDeliveredOrderItemStatus',async(req,res)=>{
@@ -41,19 +41,19 @@ router.get('/updateFbmDeliveredOrderItemStatus',async(req,res)=>{
     var startingDate=new Date();
     startingDate=startingDate.setDate(startingDate.getDate()-20)
     await updateOrderItemStatus({},{Status:'delivered',UpdatedAt:{$gte:startingDate}},8*60*60*1000);
-    res.status.send(200);
+    res.status(200).send();
 })
 
 router.get('/getAllSkus',async(req,res)=>{
     console.log("getAllSkus api called")
     await getAllSkus();
-    res.status.send(200);
+    res.status(200).send();
 })
 
 router.get('/updateAllSkus',async(req,res)=>{
     console.log("updateAllSkus api called")
     await updateAllSkus()
-    res.status.send(200);
+    res.status(200).send();
 })
 
 module.exports.schedulerRouter = router;
